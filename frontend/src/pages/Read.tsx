3a72f571b1413f652fc8434dbbdda44ed6c1a18a
Coniix <conor-evans@hotmail.com>
2023-03-08 17:30:07 +0100

    this time

--- frontend/src/pages/Read.tsx
+++ frontend/src/pages/Read.tsx
@@ -1,12 +1,13 @@
 import useSWR from "swr";
 import TableComp from "../components/TableComp";
 
-export const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:8080";
 
 export const ENDPOINT = "http://localhost:8080";
 
 
 const Read = () => {
+    const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:8080";
+
     const fetcher = (url: string) =>
     fetch(`${apiUrl}/${url}`).then((r) => r.json());
     const { data, mutate } = useSWR("actors/", fetcher);
